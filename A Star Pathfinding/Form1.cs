caf1e2cc8f833a682d1613a409a2c582732b5eba
Aidan McNabb <amcnabb67@gmail.com>
2020-09-18 17:17:04 -0700

    Add project files.

---
+++ A Star Pathfinding/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A_Star_Pathfinding
+{
+    public partial class Form1 : Form
+    {
+        const int ARRAY_WIDTH = 30;
+        const int ARRAY_HEIGHT = 20;
+
+        List<Node> open = new List<Node>();
+        List<Node> closed = new List<Node>();
+        List<Point> walls = new List<Point>();
+
+        Point start = new Point (0,0);
+        Point end = new Point(0,0);
+        bool assignFirst = true;
+
+        //public variables
+        Button[,] buttonGrid = new Button[ARRAY_WIDTH, ARRAY_HEIGHT];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            for (int i = 0; i < ARRAY_WIDTH; i++)
+            {
+                for (int j = 0; j < ARRAY_HEIGHT; j++)
+                {
+                    buttonGrid[i, j] = new Button();
+                    buttonGrid[i, j].Location = new Point(i * ClientSize.Width / ARRAY_WIDTH, j * ClientSize.Height / ARRAY_HEIGHT);
+                    buttonGrid[i, j].Size = new Size(ClientSize.Width / ARRAY_WIDTH, ClientSize.Height / ARRAY_HEIGHT);
+                    buttonGrid[i, j].Tag = new Point(i,j);
+                    buttonGrid[i, j].MouseDown += Form1_MouseDown1;
+                    buttonGrid[i, j].KeyDown += keyPress;
+                   
+                    Controls.Add(buttonGrid[i, j]);
+                }
+            }
+        }
+        private void keyPress(object sender, KeyEventArgs e)
+        {
+            Button temp = sender as Button;
+           
+            switch (e.KeyCode)
+            {
+                case Keys.Space:
+                    Pathing();
+                break;
+                
+            }
+
+            
+        }
+        private void Form1_MouseDown1(object sender, MouseEventArgs e)
+        {
+            Button temp = sender as Button;
+            switch (e.Button)
+            {
+                case MouseButtons.Left:
+                    wallRegistration(temp);
+                    break;
+                case MouseButtons.Right:
+                    endPointRegistration(temp);
+                    break;               
+
+            }
+        }
+
+        void wallRegistration(Button clickedButton)
+        {
+            Point buttonLocation = (Point)clickedButton.Tag;
+            if (walls.Contains(buttonLocation))
+            {
+                walls.Remove(buttonLocation);
+                clickedButton.BackColor = Color.Transparent;
+            }
+            else
+            {
+                walls.Add(buttonLocation);
+                clickedButton.BackColor = Color.Black;
+            }
+        }
+    
+        public void endPointRegistration(Button clickedButton)
+        { 
+            if (assignFirst)
+            {
+                buttonGrid[start.X, start.Y].BackColor = Color.Transparent;
+                start = (Point)clickedButton.Tag;
+            }
+            else
+            {
+                buttonGrid[end.X, end.Y].BackColor = Color.Transparent;
+                end = (Point)clickedButton.Tag; 
+            }
+            walls.Remove((Point)clickedButton.Tag);
+            clickedButton.BackColor = Color.Red;
+            assignFirst = !assignFirst;
+        }
+
+       
+        void Pathing()
+        {
+            if (start == end)
+                throw new Exception("Only one end point");
+
+            buttonGrid[start.X, start.Y].BackColor = Color.Red;
+            buttonGrid[end.X, end.Y].BackColor = Color.Red;
+
+            open.Add(new Node(start));
+            while(true)
+            {
+                Node current = open.First();
+                foreach (var item in open)
+                {
+                    if (item.F(start,end) < current.F(start,end))
+                        current = item;
+                }
+                open.Remove(current);
+                closed.Add(current);
+
+                if (current.Loc == end)
+                {
+                    do
+                    {
+                        buttonGrid[current.Loc.X, current.Loc.Y].BackColor = Color.Blue;
+                        current = current.parent;
+                    } while (current.Loc !=  start);
+                    break;
+                }
+                   
+                List<Node> neighbors = current.dummyNeighbors();
+                foreach (Node neighbor in neighbors)
+                {
+                    buttonGrid[neighbor.Loc.X, neighbor.Loc.Y].BackColor = Color.Yellow;
+                    if (walls.Contains(neighbor.Loc) || closed.Contains(neighbor) || neighbor.Loc.X < 0 || neighbor.Loc.Y < 0 || neighbor.Loc.X > ARRAY_WIDTH || neighbor.Loc.Y > ARRAY_HEIGHT)
+                        continue;
+
+                    if (!open.Contains(neighbor) ||
+                        open.Exists(openElement => openElement.Loc == neighbor.Loc && neighbor.F(start, end) < openElement.fCost))
+                    {
+                        neighbor.fCost = neighbor.F(start, end);
+                        neighbor.parent = current;
+                        
+                        if(!open.Contains(neighbor))
+                        {
+                            open.Add(neighbor);
+                        }
+                        else
+                        {
+                            open[open.IndexOf(open.Find(node => node.Loc == neighbor.Loc))] = neighbor;
+                        }
+                    }
+                    
+                    
+                };
+            }
+            
+        }
+
+     
+    }
+}
---
+++ A Star Pathfinding/Node.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Star_Pathfinding
+{
+    class Node
+    {
+        public Point Loc;
+        
+        public Node parent;
+        public double fCost;
+        
+ 
+        public Node(Point Loc)
+        {
+            this.Loc = Loc;
+        }
+        public List<Node> dummyNeighbors()
+        {
+            List<Node> neighbors = new List<Node>();
+            neighbors.Add(new Node(new Point(Loc.X - 1, Loc.Y - 1)));
+            neighbors.Add(new Node(new Point(Loc.X - 1, Loc.Y)));
+            neighbors.Add(new Node(new Point(Loc.X - 1, Loc.Y + 1)));
+
+            neighbors.Add(new Node(new Point(Loc.X, Loc.Y - 1)));
+            neighbors.Add(new Node(new Point(Loc.X, Loc.Y + 1)));
+
+            neighbors.Add(new Node(new Point(Loc.X + 1, Loc.Y - 1)));
+            neighbors.Add(new Node(new Point(Loc.X + 1, Loc.Y)));
+            neighbors.Add(new Node(new Point(Loc.X + 1, Loc.Y + 1)));
+
+            return neighbors;
+        }
+        
+
+        //G cost is when input = start 
+        //H cost is when output = end
+        private double nodeCost(Point input) => Math.Sqrt(Math.Pow(input.X - Loc.X, 2) + Math.Pow(input.X - Loc.Y, 2));
+        private double cost(Point input) => Math.Abs(Loc.X - input.X - Loc.Y + input.Y) + Math.Sqrt(2) * Math.Min(Math.Abs(input.X - Loc.X), Math.Abs(input.Y - Loc.Y));
+
+        //    public double Fcost(Point start, Point finish) => nodeCost(start) + nodeCost(finish);
+        public double F(Point start, Point end) => cost(start) + cost(end);
+        
+                
+
+    }   
+}
+
